d9ba7fd99c74e540d51242877fb04be87d17ed55
Hari-Sri-T <harisrishanmukhasai2005@gmail.com>
2025-03-08 19:53:38 +0530

    Created MallardDuck class Inheriting Duck Class

---
+++ MallardDuck.java
@@ -0,0 +1,12 @@
+public class MallardDuck extends Duck{
+    public MallardDuck(){
+        quackBehaviour = new Quack(); // Constructor
+        swimBehaviour = new SwimWithLegs();
+        flyBehaviour = new Fly();
+    }
+
+    @Override // method overriding
+    public void display() {
+        System.out.println("I am Mallard Duck..");
+    }
+}
