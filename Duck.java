e39dfc147da85f5662d5e338ba24bf236e8151c5
Hari-Sri-T <harisrishanmukhasai2005@gmail.com>
2025-03-08 19:52:46 +0530

    Created Duck Class

---
+++ Duck.java
@@ -0,0 +1,48 @@
+abstract public class Duck
+{
+    QuackBehaviour quackBehaviour; //Creating objects of Interfaces
+    SwimBehaviour swimBehaviour;
+    FlyBehaviour flyBehaviour;
+    // Setter and get methods
+    public void setSwimBehaviour(SwimBehaviour swimBehaviour) {
+        this.swimBehaviour = swimBehaviour;
+    }
+
+    public SwimBehaviour getSwimBehaviour() {
+        return swimBehaviour;
+    }
+
+    public void setFlyBehaviour(FlyBehaviour flyBehaviour) {
+        this.flyBehaviour = flyBehaviour;
+    }
+
+    public FlyBehaviour getFlyBehaviour() {
+        return flyBehaviour;
+    }
+
+    public void setQuackBehaviour(QuackBehaviour quackBehaviour) {
+        this.quackBehaviour = quackBehaviour;
+    }
+
+    public QuackBehaviour getQuackBehaviour() {
+        return quackBehaviour;
+    }
+
+    // Declaring abstract method
+
+    abstract public void display();
+
+    // Declaring methods
+    public void performQuack(){
+        quackBehaviour.quack();
+    }
+
+    public void performSwim(){
+        swimBehaviour.swim();
+    }
+
+    public void performFly(){
+        flyBehaviour.fly();
+    }
+}
+
