ed0733a02d5f744535ede2116991895ba273e207
Hari-Sri-T <harisrishanmukhasai2005@gmail.com>
2025-03-08 19:53:54 +0530

    Created DecoyDuck class Inheriting Duck Class

---
+++ DecoyDuck.java
@@ -0,0 +1,12 @@
+public class DecoyDuck extends Duck{
+
+    public DecoyDuck(){
+        quackBehaviour = new NoQuack(); //Constructor
+        flyBehaviour = new NoFly();
+        swimBehaviour = new Sink();
+    }
+    @Override
+    public void display() { // method overriding
+        System.out.println("I am Decoy Duck..");
+    }
+}
